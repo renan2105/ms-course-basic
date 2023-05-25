fa908e33acda5c2d574fabebeb2d2e997ac1e506
Renan <orenan124@gmail.com>
2023-05-25 08:18:11 -0300

    user client feign

---
+++ hr-oauth/src/main/java/com/renan/hroauth/services/UserService.java
@@ -0,0 +1,32 @@
+package com.renan.hroauth.services;
+
+import com.renan.hroauth.entities.User;
+import com.renan.hroauth.feignclients.UserFeignClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserService {
+
+    private static Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    @Autowired
+    private UserFeignClient userFeignClient;
+
+    public User findByEmail(String email){
+
+        User user = userFeignClient.findUserByEmail(email).getBody();
+        if(user == null){
+            throw new IllegalArgumentException("Email not found");
+        }
+
+        logger.info("Email found: " + email);
+
+        return user;
+
+    }
+
+}
